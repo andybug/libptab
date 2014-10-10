62a9e1a6871569f518e20aa90b613da5955e824c
Andrew Fields <andybug10@gmail.com>
2014-10-10 15:14:31 -0500

    Add basic Check unit testing

--- tests/tests.c
+++ tests/tests.c
@@ -1,5 +1,43 @@
 
+#include <stdlib.h>
+
+#include <check.h>
+#include <ptables.h>
+
+START_TEST (check_version)
+{
+	const char *verstr;
+
+	verstr = ptables_version();
+	ck_assert_str_eq(verstr, PTABLES_VERSION_STRING);
+}
+END_TEST
+
+Suite * version_suite(void)
+{
+	Suite *s;
+	TCase *tc_check_version;
+
+	s = suite_create("Version");
+	tc_check_version = tcase_create("check_version");
+	tcase_add_test(tc_check_version, check_version);
+	suite_add_tcase(s, tc_check_version);
+
+	return s;
+}
+
 int main(void)
 {
-	return 0;
+	Suite *s;
+	SRunner *sr;
+	int failed;
+
+	s = version_suite();
+	sr = srunner_create(s);
+
+	srunner_run_all(sr, CK_NORMAL);
+	failed = srunner_ntests_failed(sr);
+	srunner_free(sr);
+
+	return (failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
