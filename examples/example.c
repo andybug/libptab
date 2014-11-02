cbe52ee5473d334ab5f2b6ca871684507f534114
Andrew Fields <andybug10@gmail.com>
2014-11-02 15:43:09 -0600

    Add base example program

---
+++ examples/example.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <ptab.h>
+
+int main(void)
+{
+	int major, minor, patch;
+	int err;
+
+	ptab_version(&major, &minor, &patch);
+	if (major == PTAB_VERSION_MAJOR) {
+		fprintf(stderr, "libptab version mismatch:\n");
+		fprintf(stderr, "\tcompiled against: %s\n", PTAB_VERSION_STRING);
+		fprintf(stderr, "\tlinked with:      %s\n", ptab_version_string());
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
