a771d9a82b37e34a0077f09327a1a49d228a910b
Andrew Fields <andybug10@gmail.com>
2014-11-15 12:51:40 -0600

    Pass all tests except nomem

--- src/column.c
+++ src/column.c
@@ -4,5 +4,44 @@
 
 int ptab_column(ptab *p, const char *name, int flags)
 {
+	int type;
+	int align;
+
+	if (!p || !name)
+		return PTAB_ENULL;
+
+	if (!p->internal)
+		return PTAB_EINIT;
+
+	type = (PTAB_STRING | PTAB_FLOAT | PTAB_INTEGER) & flags;
+	switch (type) {
+	case PTAB_STRING:
+		break;
+
+	case PTAB_FLOAT:
+		break;
+
+	case PTAB_INTEGER:
+		break;
+
+	default:
+		return PTAB_ENUMTYPE;
+	}
+
+	align = (PTAB_ALIGN_LEFT | PTAB_ALIGN_RIGHT) & flags;
+	switch (align) {
+	case 0:
+		break;
+
+	case PTAB_ALIGN_LEFT:
+		break;
+
+	case PTAB_ALIGN_RIGHT:
+		break;
+
+	default:
+		return PTAB_ENUMALIGN;
+	}
+
 	return PTAB_OK;
 }
