9a5f035ddbe678374e8322e458ca283953824750
Andrew Fields <andybug10@gmail.com>
2014-11-16 08:39:50 -0600

    Add some commenting

--- src/column.c
+++ src/column.c
@@ -9,8 +9,14 @@ static int get_type(int flags)
 	int masked;
 	int type = -1;
 
+	/* mask out other flags */
 	masked = (PTAB_STRING | PTAB_FLOAT | PTAB_INTEGER) & flags;
 
+	/*
+	 * if only one type is set in the masked value, then set type to that
+	 * value. Otherwise, if no type was given or more than one was,
+	 * set type to -1
+	 */
 	switch (masked) {
 	case PTAB_STRING:
 	case PTAB_FLOAT:
@@ -32,8 +38,14 @@ static int get_align(int type, int flags)
 
 	assert(type > 0);
 
+	/* only want to see the alignment flags */
 	masked = (PTAB_ALIGN_LEFT | PTAB_ALIGN_RIGHT) & flags;
 
+	/*
+	 * if only one alignment was specified, set align to that value
+	 * if none were given, use the default alignment for that type
+	 * if more than one, error
+	 */
 	switch (masked) {
 	case PTAB_ALIGN_LEFT:
 	case PTAB_ALIGN_RIGHT:
@@ -77,10 +89,12 @@ int ptab_column(ptab *p, const char *name, int flags)
 
 	/* FIXME: check if num_rows > 0 */
 
+	/* get the column type from the flags */
 	type = get_type(flags);
 	if (type < 0)
 		return PTAB_ENUMTYPE;
 
+	/* get the column alignment from the flags */
 	align = get_align(type, flags);
 	if (align < 0)
 		return PTAB_ENUMALIGN;
